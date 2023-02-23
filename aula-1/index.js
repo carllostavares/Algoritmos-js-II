2f52b9a924a72e2e0757fc9d4ed00adc49382c7f
CarlosTavares <carlostavares2350@gmail.com>
2023-02-23 17:41:27 -0300

    Adicionando condicionais na funcao.

--- aula-1/index.js
+++ aula-1/index.js
@@ -1,18 +1,27 @@
-const {edGalho, edFolha} = require('./arrays');
+const {edGalho,edFolha} = require('./arrays');
 
 function juntaLissta(lista1, lista2){
     let listaFinal = [];
     let posicaoAtualLista1 = 0;
     let posicaoAtualLista2 = 0;
+    let atual = 0;
 
     while(posicaoAtualLista1 < lista1.length && posicaoAtualLista2 < lista2.length){
         let produtoAtualLista1 = lista1[posicaoAtualLista1];
         let produtoAtualLista2 = lista2[posicaoAtualLista2];
 
         if (produtoAtualLista1.preco < produtoAtualLista2.preco){
-            listaFinal[]
+            listaFinal[atual] = produtoAtualLista1
+            posicaoAtualLista1++;
+        }else{
+            listaFinal[atual] = posicaoAtualLista2;
+            posicaoAtualLista2++;
         }
+
+        atual++;
     }
 
-return listaFinal
-}
+return listaFinal;
+} 
+
+console.log(juntaLissta(edGalho,edFolha));
